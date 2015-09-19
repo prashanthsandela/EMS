4ef03edb8363ddd4a8a71c2b1b92e5225b145b59
Prashanth Sandela <prashanthsandela@gmail.com>
2015-09-19 01:13:15 -0700

    Intermidiate commit

--- EMS/src/main/java/com/comp/ems/data/mongo/MongoAdapter.java
+++ EMS/src/main/java/com/comp/ems/data/mongo/MongoAdapter.java
@@ -5,29 +5,32 @@ import java.net.UnknownHostException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.mongodb.DB;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.mongodb.MongoClient;
 
 public class MongoAdapter {
-	//------- CONSTANTS -------------------------------------------------//
+	// ------- CONSTANTS -------------------------------------------------//
 	private final String hostname = "localhost";
 	private final int port = 27017;
 	private final static String database = "local";
 	private final String username = "root";
 	private final String password = "donno";
 
-	//------- STATIC FIELDS ---------------------------------------------//
+	// ------- STATIC FIELDS ---------------------------------------------//
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(MongoAdapter.class);
 
-	//------- INSTANCE FIELDS -------------------------------------------//
+	// ------- INSTANCE FIELDS -------------------------------------------//
 	private static MongoAdapter mongoAdapter = new MongoAdapter();
 
-	//------- DYNAMIC FIELDS --------------------------------------------//
-	private static DB db;
+	// ------- DYNAMIC FIELDS --------------------------------------------//
+	private static MongoDatabase db;
 
-	//------- CONSTRUCTORS ----------------------------------------------//
-	private MongoAdapter() {}
+	// ------- CONSTRUCTORS ----------------------------------------------//
+	private MongoAdapter() {
+	}
 
 	static {
 		establishConnection();
@@ -35,22 +38,18 @@ public class MongoAdapter {
 
 	private static void establishConnection() {
 		LOGGER.debug("IN METHOD establishConnection");
-		
-		MongoClient mongoClient;
-		try {
-			mongoClient = new MongoClient( "localhost" , 27017 );
-			db = mongoClient.getDB( database );
-		} catch (UnknownHostException e) {
-			LOGGER.debug(e.toString());
-			e.printStackTrace();
-		}
+
+		MongoClient mongoClient = new MongoClient("localhost", 27017);
+		db = mongoClient.getDatabase(database);
 	}
-	
-	//------- MEMBER METHODS --------------------------------------------//
-	
+
+	// ------- MEMBER METHODS --------------------------------------------//
+
 	public static MongoAdapter getMongoAdapter() {
 		return mongoAdapter;
 	}
-	
-	public void insert(String collectionName) {}
+
+	public void insert(String collectionName, JsonNode data) {
+		
+	}
 }
